9ff381714925c0c51063f9fc57334a3700341aed
RubenCano <erreceeme20@gmail.com>
2026-01-12 12:16:46 +0100

    iniciando rama

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+Docstring for katas_fizzbuz.kata-fizzbuz_exceptions.main
+"""
+# iteracion 1:
+#   1- crear un repositiorio Fizzbuzz excepciones enviar por chat la url
+#   2- primera implementacion del problema
