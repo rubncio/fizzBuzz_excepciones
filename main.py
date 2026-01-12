8073f4d4c4c02c5bc65db92cccbd955bdf6d6c2c
RubenCano <erreceeme20@gmail.com>
2026-01-12 12:24:23 +0100

    codigo sin testeo

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 """
-Docstring for katas_fizzbuz.kata-fizzbuz_exceptions.main
+En esta kata, se solicita al usuario un numero, si este es:
+
+- Divisible entre 3, el programa debe imprimir "Fizz"
+
+- Divisible entre 5, el programa debe imprimit "Buzz"
+
+- Divisible entre 3 y entre 5, el programa debe imprimir "FizzBuzz"
+
+- Si no es divisible ni entre 3 ni entre 5, debe imprimir el número
 """
 # iteracion 1:
 #   1- crear un repositiorio Fizzbuzz excepciones enviar por chat la url
 #   2- primera implementacion del problema
+if __name__=="__main__":
+    
+    numero =input("introduzca un numero")
+    
+    if(numero%3==0 and numero%5==0):
+        print("fizzBuzz")
+    elif numero%3==0:
+        print("fizz")
+    elif(numero%5==0):
+        print("Buzz")
+    else:
+        print(numero)
