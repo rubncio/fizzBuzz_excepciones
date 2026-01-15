7400629d998ee63c8b393662b9516279f7cc7dc9
RubenCano <erreceeme20@gmail.com>
2026-01-15 12:14:01 +0100

    definiendo validaciones

---
+++ tests/test_fizzbuz.py
@@ -0,0 +1,44 @@
+from ..main import fizzbuzz
+"""En esta kata, se solicita al usuario un numero entre 1 y 100, si este es:
+
+- Divisible entre 3, el programa debe imprimir "Fizz"
+
+- Divisible entre 5, el programa debe imprimit "Buzz"
+
+- Divisible entre 3 y entre 5, el programa debe imprimir "FizzBuzz"
+
+- Si no es divisible ni entre 3 ni entre 5, debe imprimir el número
+"""
+#Validacion tipos de entrada
+def test_fizzbuzTexto():
+    #valida que el programa de error ante una entrada no numerica
+    pass
+
+#Validacion limites
+def test_fizzbuzMenorde1():
+    #valida que el programa de error ante un numero menor de 1
+    pass
+def test_fizzbuz1():
+    #valida que el programa no de error ante ante la entrada numerica 1
+    pass
+
+def test_fizzbuzMayorde100():
+    #valida que el programa de error ante un numero mayor de 100
+    pass
+def test_fizzbuz1():
+    #valida que el programa no de error ante ante la entrada numerica 100
+    pass
+
+#Validacion reglas de juego
+def test_fizzbuz3():
+    #valida que el programa devuelve fizz ante la entrada numerica 3
+    pass
+def test_fizzbuz5():
+    #valida que el programa devuelve buzz ante la entrada numerica 5
+    pass
+def test_fizzbuz15():
+    #valida que el programa devuelve fizzbuzz ante la entrada numerica 15
+    pass
+def test_fizzbuz2():
+    #valida que el programa devuelve 2 ante la entrada numerica 2
+    pass
